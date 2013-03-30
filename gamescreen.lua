3fd445cb03ed4c935b90f6cadc8ef17a98371924
Calebsem <julien.noble@ingesup.com>
2013-03-30 19:33:03 +0100

    Added stage system and enemy spawn settings

--- gamescreen.lua
+++ gamescreen.lua
@@ -12,14 +12,37 @@ function GameScreen.create()
    gamescreen.explosions = {}
    gamescreen.player = Player.create(512,384)
    gamescreen.title = "game"
-   for i = 1,10 do
-      table.insert(gamescreen.enemies, Enemy.create(math.random(30,990),math.random(30,720),math.random(1,3)))
-   end
+   gamescreen.stagenumber = 1
+   gamescreen.stagestarttimer = 5
+   gamescreen.stagewaves = 0
    return gamescreen
 end
 
 function GameScreen:update(dt)
    self.player:update(dt)
+
+   -- update explosions
+   for k,v in ipairs(self.explosions) do
+      v.radius = v.radius + 0.2
+      if v.radius >= 5 then
+         table.remove(self.explosions, k)
+      end
+   end
+
+   -- stage announcement
+   if self.stagestarttimer > 0 then
+      self.stagestarttimer = self.stagestarttimer - dt
+      return
+   end
+
+   -- check if stage ended
+   if self.stagewaves >= self.stagenumber and #self.enemies == 0 then
+      self.stagewaves = 0
+      self.stagenumber = self.stagenumber + 1
+      self.stagestarttimer = 5
+      return
+   end
+
    -- update current enemies
    for k,v in ipairs(self.enemies) do
       v:update(dt,self.player,self.explosions)
@@ -28,19 +51,15 @@ function GameScreen:update(dt)
          table.remove(self.enemies,k)
       end
    end
+
    -- randomly add enemies
    self.enemytimer = self.enemytimer - dt
-   if #self.enemies < 50 and self.enemytimer <= 0 then -- capping at 50 enemies max, lags if not
+   if self.stagewaves < self.stagenumber 
+         and ((#self.enemies < 50 and self.enemytimer <= 0) or #self.enemies == 0) then -- capping at 50 enemies max, lags if not
       self.enemytimer = math.random(5,15)
-      for i = 1,math.random(3,8) do
+      self.stagewaves = self.stagewaves + 1 -- new wave of enemies
+      for i = 1,math.random(5,10) do
          table.insert(self.enemies, Enemy.create(math.random(30,990),math.random(30,720),math.random(1,3)))
-      end
-   end
-   -- update explosions
-   for k,v in ipairs(self.explosions) do
-      v.radius = v.radius + 0.2
-      if v.radius >= 5 then
-         table.remove(self.explosions, k)
       end
    end
 end
@@ -59,4 +78,11 @@ function GameScreen:draw()
    love.graphics.setColor(50, 250, 50, 200)
    love.graphics.rectangle("fill", 10, 10, self.player.life, 5 )
    love.graphics.print(self.player.score, 10,20)
+
+   love.graphics.setColor(255,255,255,255)
+   if self.stagestarttimer > 0 then
+      love.graphics.printf("STAGE "..self.stagenumber,262, 364, 500,"center")
+   else
+      love.graphics.printf("STAGE "..self.stagenumber.." - "..self.stagewaves,262, 0, 500,"center")
+   end
 end
